8e27e06cdb1eaa7241e07cbd7f762f7f457a56d4
Simon <s.vanwageningen@uu.nl>
2023-12-06 13:00:25 +0100

    small changes (whitespaces, blank lines, imports, naming conventions)

--- visualize_metrics.py
+++ visualize_metrics.py
@@ -1,16 +1,17 @@
 import random
-
 import constants
 import visualization_tool
 import pyqtgraph as pg
 
 if __name__ == '__main__':
-    #randomize evaluation set
-    #evaluation_set = constants.evaluation_set[1:]
-    #random.shuffle(evaluation_set)
-    #constants.evaluation_set[1:] = evaluation_set
 
-    #start tool
+    # deprecated code, may be used in the future for user study
+    # randomize evaluation set
+    # evaluation_set = constants.evaluation_set[1:]
+    # random.shuffle(evaluation_set)
+    # constants.evaluation_set[1:] = evaluation_set
+
+    # start tool
     win = visualization_tool.Tool()
     win.showMaximized()
     pg.exec()
